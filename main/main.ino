e8b5f5f38d0e2541edc5753307b90010c4e5140d
Krushna <krushna.patel95@gmail.com>
2018-07-17 22:28:05 -0400

    Delayed extension of actuators after car is detected.

--- main/main.ino
+++ main/main.ino
@@ -8,6 +8,9 @@ const int actuator_y_pin = 7;
 const int light_sensor_front_pin = A0;
 const int light_sensor_back_pin = A1;
 
+const int actuator_max_extension = 2300;
+const int actuator_min_extension = 1000;
+
 int light_sensor_front_value;
 int light_sensor_back_value;
 int car_detected;
@@ -16,6 +19,9 @@ int car_detected;
 void setup(){
     Serial.begin(9600);
     actuator_x.attach(actuator_x_pin);
+    actuator_y.attach(actuator_y_pin);
+    actuator_x.writeMicroseconds(actuator_min_extension);
+    actuator_y.writeMicroseconds(actuator_min_extension);
 }
 
 void loop(){
@@ -31,6 +37,14 @@ void loop(){
     if (car_detected == 6) {
         Serial.print("Car Detected! ");
         Serial.println(car_detected);
+
+        actuator_x.writeMicroseconds(actuator_max_extension);
+        delay(6500);
+        actuator_y.writeMicroseconds(actuator_max_extension);
+        delay(7500);
+        actuator_y.writeMicroseconds(actuator_min_extension);
+        delay(6500);
+        actuator_x.writeMicroseconds(actuator_min_extension);
     }
     
     delay(500);
